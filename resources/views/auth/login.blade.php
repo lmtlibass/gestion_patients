851158776da112b52968289b5cd2d43db02d5174
libscode <lmtlibasse@gmail.com>
2023-01-27 02:09:45 +0000

    add proctfile

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,9 +12,9 @@
                         @csrf
 
                         <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
+                            
+                            <div class="col-md-6 mx-auto">
+                                <label for="email" class="col-md-4 col-form-label ">{{ __('Adress Email') }}</label>
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
@@ -26,9 +26,9 @@
                         </div>
 
                         <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
+                            
+                            <div class="col-md-6 mx-auto">
+                                <label for="password" class="col-md-4 col-form-label">{{ __('Mot de pass') }}</label>
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
                                 @error('password')
@@ -40,7 +40,7 @@
                         </div>
 
                         <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
+                            <div class="col-md-6 mx-auto">
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
@@ -52,7 +52,7 @@
                         </div>
 
                         <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
+                            <div class="col-md-6 mx-auto">
                                 <button type="submit" class="btn btn-success text-white">
                                     {{ __('Login') }}
                                 </button>
